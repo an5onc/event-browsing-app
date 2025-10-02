b6f6aa9fba44abb8adcdba7782f4a62dc1bb0f55
Ashtyn Denovellis <deno3968@bears.unco.edu>
2025-10-02 10:55:49 -0600

    test

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -47,7 +47,7 @@ const MyProfile: React.FC = () => {
     return (
       <div className="max-w-4xl mx-auto p-6">
         <h1 className="text-2xl font-bold mb-2">My profile</h1>
-        <p className="text-gray-600">Please sign in to view your profile.</p>
+        <p className="text-gray-600">Please sign in to view  your profile.</p>
       </div>
     );
   }
